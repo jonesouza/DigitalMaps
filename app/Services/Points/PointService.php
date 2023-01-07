a675245104c69111a3a44f8494079a3f234fb413
J1 <contato@jonesouza.com.br>
2023-01-07 15:35:24 -0300

    points service

---
+++ app/Services/Points/PointService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Services\Points;
+
+use App\Contracts\Repositories\Points\PointRepositoryContract;
+use App\DataTransferObjects\Points\PointSaveData;
+
+class PointService
+{
+    protected PointRepositoryContract $pointRepository;
+
+    public function __construct(PointRepositoryContract $pointRepository)
+    {
+        $this->pointRepository = $pointRepository;
+    }
+
+    public function index()
+    {
+        return $this->pointRepository->findAll();
+    }
+
+    public function store(PointSaveData $data)
+    {
+        return $this->pointRepository->create($data->toArray());
+    }
+
+    public function show(int $id)
+    {
+        return $this->pointRepository->findById($id);
+    }
+
+    public function update(int $id, PointSaveData $data)
+    {
+        return $this->pointRepository->update($id, $data->toArray());
+    }
+
+    public function destroy(int $id)
+    {
+        $this->pointRepository->delete($id);
+    }
+}
