27e30714f14f51b066f0e7c0c231371755a9785e
J1 <contato@jonesouza.com.br>
2023-01-07 14:01:27 -0300

    create abstract repository to handle with basic crud functions

---
+++ app/Repositories/Repository.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Repositories;
+
+use Illuminate\Database\Eloquent\Model;
+
+abstract class Repository
+{
+    protected Model $model;
+
+    public function findAll()
+    {
+        return $this->model->all();
+    }
+
+    public function findById(int $id)
+    {
+        return $this->model->find($id);
+    }
+
+    public function create(array $data)
+    {
+        return $this->model->create($data);
+    }
+
+    public function update(int $id, array $data)
+    {
+        $model = $this->findById($id);
+
+        if ($model) $model->update($data);
+
+        return $model;
+    }
+
+    public function delete(int $id)
+    {
+        $model = $this->findById($id);
+
+        return $model->delete();
+    }
+}
