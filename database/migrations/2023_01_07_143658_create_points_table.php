3af7e2f9ab750bb8551ecdcac188885c3a86a3e4
J1 <contato@jonesouza.com.br>
2023-01-07 11:44:03 -0300

    create points table migration

---
+++ database/migrations/2023_01_07_143658_create_points_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('points', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedInteger('x');
+            $table->unsignedInteger('y');
+            $table->timestamp('opened_at')->nullable(); 
+            $table->timestamp('closed_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('points');
+    }
+};
