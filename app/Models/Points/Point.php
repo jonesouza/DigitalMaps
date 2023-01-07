0f59883c2516e1669cf84fe1892f1671c455dfee
J1 <contato@jonesouza.com.br>
2023-01-07 16:54:43 -0300

    create score whereNear and attribute is_open to Point model

--- app/Models/Points/Point.php
+++ app/Models/Points/Point.php
@@ -18,9 +18,46 @@ class Point extends Model
         'closed_at',
     ];
 
+    protected $cast = [
+        'opened_at' => 'datetime:H:i',
+        'closed_at' => 'datetime:H:i',
+    ];
+
     public function newCollection(array $models = [])
     {
         return new PointCollection($models);
+    }
+
+    public function getIsOpenedAttribute()
+    {
+        $now = now()->format('H:i');
+
+        return $this->isOpen($now);
+    }
+
+    public function scopeWhereNear($query, int $x, int $y, int $distance)
+    {
+        return $query->where('x', '>=', $x - $distance)
+                    ->where('x', '<=', $x + $distance)
+                    ->where('y', '>=', $y - $distance)
+                    ->where('y', '<=', $y + $distance);
+    }
+
+    public function isOpen($now)
+    {
+        if ($this->opened_at === null && $this->closed_at === null) {
+            return true;
+        }
+
+        if ($this->opened_at === null && $this->closed_at !== null) {
+            return $now < $this->closed_at;
+        }
+
+        if ($this->opened_at !== null && $this->closed_at === null) {
+            return $now > $this->opened_at;
+        }
+
+        return $now > $this->opened_at && $now < $this->closed_at;
     }
 
     
